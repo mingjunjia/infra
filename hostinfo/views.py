d74d5b34e15e132e8b53a15afa4a6d1788a0e66e
mingjunjia <jmj_yc@163.com>
2018-12-21 02:41:36 -0500

    hello

--- hostinfo/views.py
+++ hostinfo/views.py
@@ -2,5 +2,28 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+import models as md
 
 # Create your views here.
+
+def show_main(request):
+    field_t = ('host_name','work_ip','map_ip', 'manage_ip', 'store_ip', 'os', 'machine_type',\
+               'status', 'in_proj')
+
+    try:
+        all_data = md.Hosts.object.values(*field_t)
+    except Exception,e:
+        return render('error.html', {"error":"at hostinfo.views.show_main "+e} )
+
+    row_list=[]
+    for i in all_data:
+        row = []
+        for j in field_t:
+            row.append(i.get(j) )
+        row_list.append(row)
+
+    return render('hostinfo/main.html','row_list':row_list)
+
+
+
+
