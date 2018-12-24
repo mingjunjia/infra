244235612aa500e41368960f7e6ccf01d1b2bf73
mingjunjia <jmj_yc@163.com>
2018-12-23 23:34:37 -0500

    hello

--- hostinfo/urls.py
+++ hostinfo/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
-from hostinfo import insert
-from hostinfo import views
+import insert
+import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^hostinfo/', include("hostinfo.urls")),
-    url(r'^main/', views.showMain),
+    #url(r'^admin/', admin.site.urls),
+    url(r'insert/', insert.insertHosts),
+    url(r'main/', views.showMain),
+    url(r'update/', views.updateHost),
 ]
--- hostinfo/views.py
+++ hostinfo/views.py
@@ -24,6 +24,50 @@ def showMain(request):
 
     return render(request, 'hostinfo/main.html',{'row_list':row_list})
 
+def updateHost(request):
+    if request.method == "GET":
+        return render(request, "hostinfo/update_host.html")
+
+    if "host_name" in request.POST and request.POST["host_name"] != '':
+        if len( md.Hosts.objects.filter(host_name=request.POST["host_name"])) == 0:
+            notify = "no update data: the host not exist, please fore insert."
+            return render(request, "hostinfo/update_host.html", {"status":notify} )
+
+        field_l = ['work_ip','map_ip', 'manage_ip', 'store_ip', 'os', 'machine_type',\
+               'status', 'in_proj']
+        try:
+            for i in field_l:
+                   if (i not in request.POST) or request.POST[i] == "":
+                       field_l.remove(i)
+            field_d = {}
+
+            for i in field_l:
+                field_d.update( { i:request.POST[i] } )
+
+            md.Hosts.objects.filter(host_name=request.POST["host_name"]).update(**field_d)
+        except Exception,e:
+               return render(request, "hostinfo/update_host.html", {"error":"hostinfo/views \
+                                                                    updateHost:" + str(e)} )
+        return render(request, "hostinfo/update_host.html",{"status":"success update."})
 
 
+    else:
+        return render(request, "hostinfo/update_host.html",{"status":"host name is required"})
 
+
+def deleteHost(request):
+    if request.method == "GET":
+        return render(request, "hostinfo/update_host.html")
+
+    if "host_name" in request.POST and request.POST["host_name"] != '':
+        if len( md.Hosts.objects.filter(host_name=request.POST["host_name"])) == 0:
+            notify = "no update data: the host not exist, please fore insert."
+            return render(request, "hostinfo/update_host.html", {"status":notify} )
+
+    try:
+        host = md.Hosts.objects.get(host_name=request.POST['host_name'])
+        host.delete()
+    except Exception,e:
+
+        return render(request, "hostinfo/update_host.html", {"error":"hostinfo/views \
+                                                             deleteHost: failed +"+ str(e)})
